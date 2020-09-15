47f977df322fd0335ae92f2f4fd6a1411f3cf1eb
陶清晨 <776404387@qq.com>
2020-09-15 20:02:31 +0800

    #dubbo project

---
+++ emall-api/a901/src/main/java/cn/com/sinosoft/a901/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cn.com.sinosoft.a901;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(A901Application.class);
+    }
+
+}
