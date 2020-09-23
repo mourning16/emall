c1719b3b4ceb718f626a2e4dcf36d0ca1af99ed7
陶清晨 <776404387@qq.com>
2020-09-23 13:10:50 +0800

    # dubbo

--- emall-api/a901/src/main/java/cn/com/sinosoft/a901/A901Application.java
+++ emall-api/a901/src/main/java/cn/com/sinosoft/a901/A901Application.java
@@ -2,12 +2,20 @@ package cn.com.sinosoft.a901;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class A901Application {
+public class A901Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(A901Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(A901Application.class);
     }
 
 }
--- emall-interface-service/s200/src/main/java/cn/com/sinosoft/s200/S200Application.java
+++ emall-interface-service/s200/src/main/java/cn/com/sinosoft/s200/S200Application.java
@@ -2,12 +2,20 @@ package cn.com.sinosoft.s200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class S200Application {
+public class S200Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(S200Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(S200Application.class);
     }
 
 }
