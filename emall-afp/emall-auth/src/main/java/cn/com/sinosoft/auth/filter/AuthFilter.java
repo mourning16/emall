8bd03a54514144135086ae9e48a9940793433a19
陶清晨 <776404387@qq.com>
2020-11-12 14:48:51 +0800

    @dubbo project

---
+++ emall-afp/emall-auth/src/main/java/cn/com/sinosoft/auth/filter/AuthFilter.java
@@ -0,0 +1,25 @@
+package cn.com.sinosoft.auth.filter;
+
+import com.alibaba.dubbo.common.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.*;
+
+/**
+ * @Description TODO
+ * @Author Mourning 16
+ * @Date 2020/11/12 13:59
+ * @Version 1.0
+ */
+@Slf4j
+@Activate(group= {Constants.CONSUMER, Constants.PROVIDER},order = -3000)
+public class AuthFilter implements Filter {
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+
+        log.info("是否为生产者："+RpcContext.getContext().isProviderSide());
+        return invoker.invoke(invocation);
+
+    }
+}
